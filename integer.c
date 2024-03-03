abf6a11155b29ddb0dc20a49f016e09dad4d056f
VengeanceIsHis <143756595+VengeanceIsHis@users.noreply.github.com>
2024-03-03 14:43:19 -0600

    More bug fixes

--- integer.c
+++ integer.c
@@ -11,9 +11,8 @@ int print_d(va_list args)
     int k;
 
     if (num == INT_MIN) {
-        // Handling for INT_MIN
         char min[] = "-2147483648";
-        int len = 11; // Length of INT_MIN string
+        int len = 11;
         fwrite(min, sizeof(char), len, stdout);
         return len;
     } else {
@@ -22,26 +21,24 @@ int print_d(va_list args)
             num = -num;
         }
 
-        // Convert the number to string
+
         do {
             int digit = num % 10;
             buffer[i++] = digit + '0';
             num /= 10;
         } while (num != 0);
 
-        // Add '-' sign if the number was negative
+
         if (is_negative) {
             buffer[i++] = '-';
         }
 
-        // Reverse the string
         for (k = 0; k < i / 2; k++) {
             char temp = buffer[k];
             buffer[k] = buffer[i - k - 1];
             buffer[i - k - 1] = temp;
         }
 
-        // Write the string to stdout
         fwrite(buffer, sizeof(char), i, stdout);
         return i;
     }
