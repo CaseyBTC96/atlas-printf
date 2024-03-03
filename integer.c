9ada82b8cc65f83cacca60fb0209ae5577fa8ce0
VengeanceIsHis <143756595+VengeanceIsHis@users.noreply.github.com>
2024-03-03 12:26:27 -0600

    Fixing problem with negative numbers

--- integer.c
+++ integer.c
@@ -1,29 +1,36 @@
 #include "main.h"
 
 int print_d(va_list args)
-{
+ {
     int num = va_arg(args, int);
     char buffer[20];
     int i = 0;
-    int j;
+    int is_negative = 0;
 
-    while (num != 0) {
+    if (num < 0) {
+        is_negative = 1;
+        num = -num;
+    }
+
+    do {
         int digit = num % 10;
         buffer[i++] = digit + '0';
         num /= 10;
-    }
-    if (i == 0) {
-        buffer[i++] = '0';
+    } while (num != 0);
+
+    if (is_negative) {
+        buffer[i++] = '-';
     }
 
-    for (j = 0; j < i / 2; j++)
-    {
+    for (int j = 0; j < i / 2; j++) {
         char temp = buffer[j];
         buffer[j] = buffer[i - j - 1];
         buffer[i - j - 1] = temp;
     }
 
-
     fwrite(buffer, sizeof(char), i, stdout);
-    return i; 
+    return i;
 }
+
+
+
