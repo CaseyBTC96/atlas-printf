1b428e1ac8f924ffaef9ba825cadae699b7aa7cc
casey Paul <casey.paul@atlasschool.com>
2024-06-26 20:48:41 +0000

    started main.h file

---
+++ main.h
@@ -0,0 +1,17 @@
+#ifndef our_printf
+#define our_printf
+#include <stdio.h>
+#include <stdarg.g>
+/**
+ * struct specifier - struct specifier.
+ * @valid: the valid character.
+ * @f: the function associated.
+ *
+ */
+typedef struct specifier
+{
+	char *valid;
+	int (*f)(va_list);
+}  spec;
+int _printf(const char *format, ...);
+#endif 
