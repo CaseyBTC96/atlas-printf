51bb73846a2f6dae6a877c723baec012c5e69d6d
Casey Paul <casey.paul@atlasschool.com>
2024-06-29 17:54:18 +0000

    added print_d and print_i to get_func and main.h

--- main.h
+++ main.h
@@ -17,6 +17,8 @@ int _printf(const char *format, ...);
 int _putchar(char c);
 int print_c(va_list args);
 int print_s(va_list args);
+int print_d(va_list args);
+int print_i(va_list args);
 int print_percent(va_list args);
 int (*get_func(char x))(va_list args);
 #endif
