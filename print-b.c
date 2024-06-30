783fa426e84b507ed5869545b336479526a7fe96
Tsimmuaj <tyang4204@gmail.com>
2024-06-30 12:50:29 -0500

    initial binary convertion

--- print-b.c
+++ print-b.c
@@ -6,7 +6,20 @@
 */
 int print_b(va_list args)
 {
-	int binaryprint[25];
+	int binaryprint[25], number_check = args, loop = 0;
 
-	
+	while ((number_check) != 0)
+	{
+		if ((number_check % 2) == 1)
+			binaryprint[loop] = 1;
+		else
+			binaryprint[loop] = 0;
+		loop++;
+		number_check = number_check / 2;
+	}
+	while (loop > 0)
+	{
+		_putchar(binaryprint[loop] + '0');
+		loop--;
+	}
 }
