04fe03734378d4d0140b5fcb12d40c2a13fe9653
Tsimmuaj <tyang4204@gmail.com>
2024-06-30 13:08:22 -0500

    fixed statement issue

--- print-b.c
+++ print-b.c
@@ -8,16 +8,19 @@ int print_b(va_list args)
 {
 	int binaryprint[25], number_check = va_arg(args, int), loop = 0;
 
-	if (!number_check)
+	if (!number_check)/*checks for unsinged int*/
 		return (0);
-	while ((number_check) != 0)
+	while (1)
 	{
 		if ((number_check % 2) == 1)
 			binaryprint[loop] = 1;
 		else
 			binaryprint[loop] = 0;
-		loop++;
 		number_check = number_check / 2;
+		if (number_check == 0)
+			break;
+		else
+			loop++;
 	}
 	while (loop != 0)
 	{
