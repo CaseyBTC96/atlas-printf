61c1da24fb771e145a5c9cc06638741b282ae577
Tsimmuaj <tyang4204@gmail.com>
2024-06-30 13:01:05 -0500

    intitial idea of b

--- print-b.c
+++ print-b.c
@@ -1,13 +1,15 @@
 #include "main.h"
 /**
- * print-b - converts unsigned int to binary
+ * print_b - converts unsigned int to binary
  * @args: the number
  * Return: binary and 0 on false.
 */
 int print_b(va_list args)
 {
-	int binaryprint[25], number_check = args, loop = 0;
+	int binaryprint[25], number_check = va_arg(args, int), loop = 0;
 
+	if (!number_check)
+		return (0);
 	while ((number_check) != 0)
 	{
 		if ((number_check % 2) == 1)
@@ -17,9 +19,11 @@ int print_b(va_list args)
 		loop++;
 		number_check = number_check / 2;
 	}
-	while (loop > 0)
+	while (loop != 0)
 	{
 		_putchar(binaryprint[loop] + '0');
 		loop--;
 	}
+	_putchar(binaryprint[loop] + '0');
+	return (1);
 }
